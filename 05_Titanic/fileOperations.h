8a5bbf85be8b1528682fb873e3034b6110f67eec
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2023-11-21 08:17:45 +0100

    Delete 05_Titanic directory

--- 05_Titanic/fileOperations.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_05_TITANIC_FILEOPERATIONS_H
-#define INC_05_TITANIC_FILEOPERATIONS_H
-
-bool openFiles(const std::string &inputFileName, const std::string &outputFileName,
-               std::ifstream &inputFile, std::ofstream &outputFile);
-
-void closeFiles(std::ifstream &inputFile, std::ofstream &outputFile);
-
-#endif //INC_05_TITANIC_FILEOPERATIONS_H
--- 05_Titanic/userInputs.h
+++
@@ -1,8 +0,0 @@
-#ifndef INC_05_TITANIC_USERINPUTS_H
-#define INC_05_TITANIC_USERINPUTS_H
-
-void handleReadError();
-
-char getUserChoice(const std::string& text, char choice1, char choice2);
-
-#endif //INC_05_TITANIC_USERINPUTS_H
